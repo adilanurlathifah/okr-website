859560de7ebf76ee7b371591d11f3f7d937755e8
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2024-03-04 09:16:06 +0700

    fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,5 @@
-export default function ({ store, redirect, $auth }) {
-    if (!$auth.loggedIn) {
-      return redirect('/login');
-    }
-  }
-
-export const actions =  {
-  async login({ commit }, loginData) {
-    try {
-      const response = await this.$axios.$post('/api/auth/admin/login', loginData);
-      this.$auth.setToken('local', 'Bearer ' + response.data.token);
-      await this.$auth.fetchUser();
-      commit('setAuthData', response.data);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-}
+// export default function ({ store, redirect, $auth }) {
+//   if (!$auth.loggedIn) {
+//     return redirect("/login");
+//   }
+// }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,86 +1,109 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'okr-website',
+    title: "okr-website",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~plugins/v-calendar.js', ssr: false },
-    { src: '~/plugins/vue-tailwind.client.js'},
-    { src: '~/plugins/vue-sweetalert2.js'},
+    { src: "~plugins/v-calendar.js", ssr: false },
+    { src: "~/plugins/vue-tailwind.client.js" },
+    { src: "~/plugins/vue-sweetalert2.js" },
     { src: "@/services/get-access-token", mode: "client" },
-    { src: '~/plugins/axios'}
+    { src: "~/plugins/axios" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/google-fonts'
-  ],
+  buildModules: ["@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
       Roboto: true,
-      Montserrat: '200..800'
-    }
+      Montserrat: "200..800",
+    },
   },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/auth',
-    '@nuxtjs/axios',
-    '@nuxtjs/tailwindcss',
-    'vue-sweetalert2/nuxt'
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
+    "@nuxtjs/tailwindcss",
+    "vue-sweetalert2/nuxt",
   ],
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: '/api/auth/admin/login', method: 'post', propertyName: 'data.token' },
-          logout: { url: '/api/auth/admin/logout', method: 'get' },
-          user: { url: '/api/user', method: 'get', propertyName: 'data' }
-        },
-        tokenRequired: true,
-        tokenType: 'Bearer'
-      }
-    },
-    redirect: {
-      login: '/login',
-      logout: '/login',
-      home: '/admin'
-    }
-  },
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://192.168.18.50:8000',
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+    },
+    baseURL: process.env.HOST_API,
+    progress: false,
+    proxy: true,
   },
+  proxy: {
+    "/api/": process.env.HOST_API || "",
+  },
+  auth: {
+    watchLoggedIn: true,
+    redirect: {
+      login: "/login",
+      logout: "/login",
+      callback: "/",
+      home: "/admin",
+    },
+    localStorage: {
+      prefix: "pw.",
+    },
+    token: {
+      prefix: "_token.",
+      global: true,
+    },
+    strategies: {
+      localUser: {
+        scheme: "local",
+        token: {
+          property: "data.token",
+          global: true,
+          type: "Bearer",
+          maxAge: 172800,
+        },
+        user: {
+          property: "data",
+        },
+        endpoints: {
+          login: {
+            url: "/api/auth/admin/login",
+            method: "post",
+          },
+          user: { url: "/api/user", method: "get" },
+          logout: { url: "/api/auth/admin/logout", method: "get" },
+        },
+      },
+    },
+  },
+
   router: {
     extendRoutes(routes, resolve) {
       routes.push({
-          name: 'admin',
-          path: '/',
-          component: resolve(__dirname, 'pages/admin/index.vue'),
-        });
+        name: "admin",
+        path: "/",
+        component: resolve(__dirname, "pages/admin/index.vue"),
+      });
     },
     // middleware: ['auth'],
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
